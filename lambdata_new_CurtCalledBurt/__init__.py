c8a89c79597439db660d7bc73a4c6c0fbfff5ea7
CurtCalledBurt <CurtCalledBurt@users.noreply.github.com>
2019-10-28 14:02:44 -0700

    added df_utils.py and init  files with test functions

---
+++ lambdata_new_CurtCalledBurt/__init__.py
@@ -0,0 +1,14 @@
+"""
+Some helpful code snippets
+"""
+
+import numpy as np 
+import pandas as pd 
+
+#sample code lines, dataframes
+ONES = pd.DataFrame(np.ones(10))
+ZEROES = pd.DataFrame(np.zeros(20))
+
+#sample code, functions
+def increment(x):
+    return(x + 1)
---
+++ lambdata_new_CurtCalledBurt/df_utils.py
@@ -0,0 +1,6 @@
+"""
+utility functions for pandas dataframes
+"""
+
+import pandas as pd
+test_df = pd.DataFrame([1,2,3,4,5])
